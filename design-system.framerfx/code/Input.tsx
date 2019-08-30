c044c759ad8a55f2b74cc8f375b20de29ae0c2cc
Luis Ricardo La Torre <luisrlatorre@gmail.com>
2019-08-29 22:11:01 -0400

    yarn once again

--- design-system.framerfx/code/Input.tsx
+++ design-system.framerfx/code/Input.tsx
@@ -3,44 +3,44 @@ import * as System from "../../design-system"
 import { ControlType, PropertyControls } from "framer"
 
 type Props = System.InputProps & {
-  width: number
-  height: number
+    width: number
+    height: number
 }
 
 type State = {
-  value?: string
+    value?: string
 }
 
 export class Input extends React.Component<Props, State> {
-  state = {
-    value: null
-  }
+    state = {
+        value: "",
+    }
 
-  handleChange = event => {
-    this.setState({ value: event.target.value })
-  }
+    handleChange = event => {
+        this.setState({ value: event.target.value })
+    }
 
-  render() {
-    return (
-      <System.Input
-        {...this.props}
-        value={this.state.value}
-        onChange={this.handleChange}
-      />
-    )
-  }
+    render() {
+        return (
+            <System.Input
+                {...this.props}
+                value={this.state.value}
+                onChange={this.handleChange}
+            />
+        )
+    }
 
-  static defaultProps: Props = {
-    width: 150,
-    height: 50,
-    disabled: false,
-    error: false,
-    placeholder: "Email"
-  }
+    static defaultProps: Props = {
+        width: 150,
+        height: 50,
+        disabled: false,
+        error: false,
+        placeholder: "Email",
+    }
 
-  static propertyControls: PropertyControls<Props> = {
-    placeholder: { type: ControlType.String, title: "Placeholder" },
-    disabled: { type: ControlType.Boolean, title: "Disabled" },
-    error: { type: ControlType.Boolean, title: "Error" }
-  }
+    static propertyControls: PropertyControls<Props> = {
+        placeholder: { type: ControlType.String, title: "Placeholder" },
+        disabled: { type: ControlType.Boolean, title: "Disabled" },
+        error: { type: ControlType.Boolean, title: "Error" },
+    }
 }
